4d946bb99993843f4afc1d471bd65eb2ca7b3021
OzymandiasKnight <ozymandias.knight@gmail.com>
2025-10-10 19:45:48 +0200

    Adding NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+        <>
+            {
+                //On utilise className a la place de class en react
+            }
+            <nav className="nav-bar">
+                <h1>Site de l'ESGI</h1>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar;
